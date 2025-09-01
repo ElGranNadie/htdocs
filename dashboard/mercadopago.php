c80646b07ad97dbdd7003c8d751c0e4ca92ef1ea
ElGranNadie <95330873+ElGranNadie@users.noreply.github.com>
2025-08-31 19:15:11 -0500

    Update mercadopago.php
    cague subiendo por error el token :V

--- dashboard/mercadopago.php
+++ dashboard/mercadopago.php
@@ -4,4 +4,4 @@ require __DIR__ . '/../vendor/autoload.php';
 use MercadoPago\MercadoPagoConfig;
 
 // 🔑 Access Token (aquí pegas el tuyo)
-MercadoPagoConfig::setAccessToken("APP_USR-2420974097835435-082501-fe73a9231de6ac5a5c13ee527e9724a9-2649569840");
+MercadoPagoConfig::setAccessToken("x");
